368dc4d272abacb3b15dd504091718f6f1a42bef
Elshan Jayathissa <email.elshan2003@gmail.com>
2025-07-16 10:00:57 +0530

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "busgo";
+$host = "sql8.freesqldatabase.com";
+$user = "sql8790262";
+$pass = "2cVQq9tUrD";
+$db = "sql8790262";
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
